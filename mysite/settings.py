11f17539522c9d7676bd663905ed63eee8a27db0
Daniel Hedlund <daniel,hedlund@codemill.se>
2016-11-16 17:06:22 +0100

    Set ALLOWED_HOSTS

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '-5c77z-3g96(+uzkx+ud$+-z&!fhgi2mery@(^=^r4(udqc4q0'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['dhd.pythonanywhere.com',]
 
 
 # Application definition
